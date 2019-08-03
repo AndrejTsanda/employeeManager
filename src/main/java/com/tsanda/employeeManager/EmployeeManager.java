8dc5b0230a69f8c959517ed031d339d523f4e52d
AndrejTsanda <tsanda.andrej@gmail.com>
2019-08-03 23:02:10 +0300

    Initial project. Main Class.

--- src/main/java/com/tsanda/employeeManager/EmployeeManager.java
+++ src/main/java/com/tsanda/employeeManager/EmployeeManager.java
@@ -1,4 +1,12 @@
 package com.tsanda.employeeManager;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class EmployeeManager {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManager.class, args);
+    }
 }
